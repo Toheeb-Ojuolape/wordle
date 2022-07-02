4899c04b1eb434ba17aa99fd64ddf11b54ffc653
Toheeb Ojuolape <45619240+Toheeb-Ojuolape@users.noreply.github.com>
2022-07-02 19:02:22 +0100

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -34,7 +34,7 @@ function Home() {
           const randomSolution = json[Math.floor(Math.random()*json.length)]
           if(savedSolution == null){
           setSolution(randomSolution.word)
-          localStorage.setItem('solution', "solve")
+          localStorage.setItem('solution', randomSolution.word)
           }
           else{
           setSolution(savedSolution)
